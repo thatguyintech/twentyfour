c3207a31e0f5f205bd7ece1ab227cd1f40aacd44
Allan Peng <potette007@gmail.com>
2014-10-04 07:07:59 -0700

    implemented shuffling

--- static/js/generate-nums.js
+++ static/js/generate-nums.js
@@ -1,7 +1,13 @@
+function notEmpty(str) {
+    return str.length>0;
+
+}
 $.get("etc/valid_combos.txt", function(data) {
     var rows = data.split("\n");
     var random = Math.floor(Math.random() * rows.length);
     var nums = rows[random];
+    nums = nums.split(" ");
+    nums = nums.filter(notEmpty);
     // shuffle
     for (var i = 0; i < 4; i++) { 
         var j = Math.floor(Math.random() * (4 - i) + i);
@@ -9,6 +15,12 @@ $.get("etc/valid_combos.txt", function(data) {
         nums[j] = nums[i];
         nums[i] = temp;
     }
-    $("#randnums").text(nums);
+    var given_nums = "";
+    for ( var i = 0; i < 4; i++) {
+        given_nums+=nums[i];
+        given_nums+= " ";
+    }
+   // $("#randnums").text(nums);
+    $("#randnums").text(given_nums);
 });
 
