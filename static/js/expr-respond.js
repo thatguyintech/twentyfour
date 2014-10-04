3161948c8fb0c6e3a3dbe0b062ea9243dfc9dae1
Ronald Kwan <rkwan@berkeley.edu>
2014-10-04 04:03:59 -0700

    allow spaces in expression

--- static/js/expr-respond.js
+++ static/js/expr-respond.js
@@ -9,7 +9,8 @@ $("#expr-submit").click(function() {
     var expr = $("#expr-input").val();
     if (expr.length > 0) {
         try {
-            var exprval = parser.parse(expr);
+            var space_re = new RegExp("\\s+", "g");
+            var exprval = parser.parse(expr.replace(space_re, ""));
             var epsilon = 0.000001;
             if (Math.abs(exprval - 24) < epsilon) {
                 alert("Good job!");
